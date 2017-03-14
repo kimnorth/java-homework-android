e9c6cb14f4c6467680f7324bea37a5334e313c62
kimnorth <benignhumour@gmail.com>
2017-03-14 22:20:53 +0000

    Got my mvp working. The answer activity now displays the total number of words in a passed string.

--- app/src/main/java/com/codeclan/wordcounter/AnswerActivity.java
+++ app/src/main/java/com/codeclan/wordcounter/AnswerActivity.java
@@ -1,13 +1,36 @@
 package com.codeclan.wordcounter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class AnswerActivity extends AppCompatActivity {
+
+    WordCounter wordCounter;
+    TextView answerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+
+        wordCounter = new WordCounter();
+
+
+
+        String phrase = extras.getString("phrase");
+        wordCounter.addPhrase(phrase);
+        int words = wordCounter.getWordListLength();
+
+        answerText = (TextView)findViewById(R.id.answer_text);
+        answerText.setText(phrase + ": " + words + " words");
     }
+
+
 }
